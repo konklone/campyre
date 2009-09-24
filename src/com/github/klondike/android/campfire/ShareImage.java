52ac3d5ab2da88e86c9847a73881b6ef533664e2
klondike <kprojection@gmail.com>
2009-09-23 23:49:58 -0400

    Added a couple controls

--- src/com/github/klondike/android/campfire/ShareImage.java
+++ src/com/github/klondike/android/campfire/ShareImage.java
@@ -3,10 +3,14 @@ package com.github.klondike.android.campfire;
 import java.io.File;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ShareImage extends Activity {
 	private File image;
+	private TextView explain;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -14,16 +18,19 @@ public class ShareImage extends Activity {
 		
 		setContentView(R.layout.share);
 		
-		loadImage();
 		setupControls();
+		loadImage();
 	}
 	
 	public void loadImage() {
-		
+		Bundle extras = this.getIntent().getExtras();
+		Uri contentURI = (Uri) extras.get("android.intent.extra.STREAM");
+		explain.setText(contentURI.toString());
 	}
 	
 	public void setupControls() {
-		
+		explain = (TextView) this.findViewById(R.id.explain);
+		Button upload = (Button) this.findViewById(R.id.upload);
 	}
 	
 	public void uploadImage() {
