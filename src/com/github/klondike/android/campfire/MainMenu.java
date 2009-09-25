25bf57d83faea9022a601614cd5b6a4b8b268d59
klondike <kprojection@gmail.com>
2009-09-24 23:31:12 -0400

    Moved speaking out of the UI thread

--- src/com/github/klondike/android/campfire/MainMenu.java
+++ src/com/github/klondike/android/campfire/MainMenu.java
@@ -1,23 +1,29 @@
 package com.github.klondike.android.campfire;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.github.klondike.android.campfire.R;
 import com.github.klondike.java.campfire.Campfire;
 import com.github.klondike.java.campfire.CampfireException;
 
-public class MainMenu extends Activity {   
+public class MainMenu extends Activity {
+	private static final int LOGGING_IN = 0;
+	private static final int SPEAKING = 1;
+	
 	private Campfire campfire;
 	private String roomId;
 	private TextView out;
 	private EditText message;
 	private Button speak;
+	private boolean spoke;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -45,6 +51,18 @@ public class MainMenu extends Activity {
         out.setText(output);
     }
     
+    final Handler handler = new Handler();
+    final Runnable updateSpeaking = new Runnable() {
+        public void run() {
+        	if (spoke)
+        		out.setText("Spoke to room!\n");
+    		else
+    			out.setText("Didn't speak to room :(\n");
+        	message.setText("");
+        	dismissDialog(SPEAKING);
+        }
+    };
+    
     public void setupControls() {
     	out = (TextView) findViewById(R.id.debug);
         speak = (Button) this.findViewById(R.id.speak);
@@ -52,17 +70,25 @@ public class MainMenu extends Activity {
         
     	speak.setOnClickListener(new View.OnClickListener() {
     		public void onClick(View v) {
-    			try {
-	    			if (campfire.speak(message.getText().toString(), roomId))
-	        			out.setText("Spoke to room!\n");
-	        		else
-	        			out.setText("Didn't speak to room :(\n");
-    			} catch (CampfireException e) {
-    				out.setText("Error speaking to room :(\n");
-    			}
-    			message.setText("");
+    			speak();
     		}
     	});
+    }
+    
+    public void speak() {
+    	Thread speakingThread = new Thread() {
+    		public void run() {
+    			try {
+    				spoke = campfire.speak(message.getText().toString(), roomId);
+	    		} catch (CampfireException e) {
+	    			spoke = false;
+				}
+	    		handler.post(updateSpeaking);
+    		}
+    	};
+    	speakingThread.start();
+    	
+    	showDialog(SPEAKING);
     }
     
     public void loadCampfire() {
@@ -78,6 +104,18 @@ public class MainMenu extends Activity {
         	ssl = true;
         
         campfire = new Campfire(username, email, password, ssl);
+    }
+    
+    protected Dialog onCreateDialog(int id) {
+        switch(id) {
+        case SPEAKING:
+            ProgressDialog dialog = new ProgressDialog(this);
+            dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            dialog.setMessage("Speaking...");
+            return dialog;
+        default:
+            return null;
+        }
     }
     
 }
