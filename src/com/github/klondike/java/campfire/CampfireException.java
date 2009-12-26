3032a6b34ec385790346b252dd0b9b760761259d
klondike <kprojection@gmail.com>
2009-12-26 03:34:53 -0500

    Allowed arbitrary setting and getting of messages onto CampfireExceptions

--- src/com/github/klondike/java/campfire/CampfireException.java
+++ src/com/github/klondike/java/campfire/CampfireException.java
@@ -2,6 +2,8 @@ package com.github.klondike.java.campfire;
 
 public class CampfireException extends Exception {
     private static final long serialVersionUID = -2623309261327198087L;
+    private String msg;
+    
     
     public CampfireException(Exception e) {
     	super(e);
@@ -9,5 +11,15 @@ public class CampfireException extends Exception {
     
     public CampfireException(String msg) {
     	super(msg);
+    	this.msg = msg;
+    }
+    
+    public CampfireException(Exception e, String msg) {
+    	super(e);
+    	this.msg = msg;
+    }
+    
+    public String getMessage() {
+    	return this.msg;
     }
 }
